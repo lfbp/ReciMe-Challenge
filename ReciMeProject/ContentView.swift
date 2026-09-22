82210f05bb6b20299032e959377104b60b79e644
Luis Pereira <luisfelipe.b.pereira@gmail.com>
2026-09-22 01:23:56 -0300

    Initial Commit

---
+++ ReciMeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReciMeProject
+//
+//  Created by Luis Pereira on 22/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReciMeProject/ReciMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciMeProjectApp.swift
+//  ReciMeProject
+//
+//  Created by Luis Pereira on 22/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ReciMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
